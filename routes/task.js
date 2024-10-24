fe862a8a6f41cfb63b527e65269d5b414b770f2e
Narjisha K <144308345+NarjishaK@users.noreply.github.com>
2024-10-24 16:12:21 +0530

    suggest name

--- routes/task.js
+++ routes/task.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const Controller = require('../controller/task')
+const Admin = require('../models/admins')
 //routes for task
 
 router.post('/', Controller.createTask);
@@ -9,6 +10,12 @@ router.get('/:id', Controller.getTask);
 router.put('/:id', Controller.updateTask);
 router.delete('/:id', Controller.deleteTask);
 router.delete('/', Controller.deleteAllTasks);
-
-
+router.get('/admins', async (req, res) => {
+    try {
+      const admins = await Admin.find({}, 'name email'); 
+      res.json(admins);
+    } catch (error) {
+      res.status(500).json({ message: 'Error fetching admins' });
+    }
+  });
 module.exports = router;
